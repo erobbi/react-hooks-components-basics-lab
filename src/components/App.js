81b6b96b0717e295fda243cc51c54e9cac559c07
erobbi <elirobbins0@gmail.com>
2021-07-18 22:17:28 -0400

    completed assingnment

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>What should I do here???</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home /> 
+      <About />
     </div>
   );
 }
